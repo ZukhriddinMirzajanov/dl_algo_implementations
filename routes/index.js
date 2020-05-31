b11cad930d5c3990180abf5d73910f68b5ed9f24
Zukhriddin <lookslike_98@outlook.com>
2020-05-31 19:01:09 +0200

    Updated

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Salom");
+  res.render("index");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+if (process.env.DATABASE_URL !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
 
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/viewa");
+app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to mongoDB..."))
+  .catch((err) => console.log("Error during the connection ", err));
+
 app.use("/", indexRouter);
+
 app.listen(process.env.PORT || 2000);
