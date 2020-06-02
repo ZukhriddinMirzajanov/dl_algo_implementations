027624923aba7f969688779a404ed52c297bb67e
Zukhriddin <lookslike_98@outlook.com>
2020-06-02 16:41:51 +0200

    Initial Product Routes

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+
 const indexRouter = require("./routes/index");
+const productsRouter = require("./routes/products");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 
@@ -24,5 +28,6 @@ mongoose
   .catch((err) => console.log("Error during the connection ", err));
 
 app.use("/", indexRouter);
+app.use("/products", productsRouter);
 
 app.listen(process.env.PORT || 2000);
